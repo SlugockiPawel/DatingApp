9484d728066a222219daaa22656b9b3706e35a93
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-23 17:08:01 +0200

    implement GetUsersAsync and GetUserAsync

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using DatingApp.Data;
+using DatingApp.Models;
+using DatingApp.Repos.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IAppUserRepo _userRepo;
+
+        public UsersController(IAppUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
+        {
+            var users = await _userRepo.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserAsync(Guid id)
+        {
+            var user = await _userRepo.GetUserByIdAsync(id);
+            return user is not null ? Ok(user) : NotFound();
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
