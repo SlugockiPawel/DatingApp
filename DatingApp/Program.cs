3c9622f27f5d96be2efb4d5b288c5391a0768988
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-23 15:04:23 +0200

    add postgres connection string, create migration and update database

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -1,3 +1,6 @@
+using DatingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"),
+        o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
