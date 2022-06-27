a6c889ce17e40c815a986766fa16a78d3371a6d2
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-27 11:17:59 +0200

    add PasswordHash and PasswordSalt to AppUser model (and update-database)

--- DatingApp/Models/AppUser.cs
+++ DatingApp/Models/AppUser.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
