fd6308ac05f288967d199d4d8395d70ac899f36a
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-28 12:58:27 +0200

    add nav component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
