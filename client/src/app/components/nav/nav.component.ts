c2e8751da1b4e866d5812580cd78ec1ba309680e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-29 13:25:07 +0200

    change persist login logic to use async pipe

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/_models/user';
 import { AccountService } from '../../_services/account.service';
 
 @Component({
@@ -8,19 +10,18 @@ import { AccountService } from '../../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn!: boolean;
 
-  constructor(private accountService: AccountService) { }
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error),
     })
@@ -28,13 +29,5 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      next: this.loggedIn = !!user;
-      error: (error: Error) => console.log(error);
-    })
   }
 }
