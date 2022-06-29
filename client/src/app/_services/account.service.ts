b118f28600ed901d0f5c670cd178ccf3cf5304cf
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-29 12:10:07 +0200

    implement mechanism to persis login user
    when browser is refreshed => user is still logged in

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  private baseurl: string = 'https://localhost:5001/api/'
+  private baseurl: string = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseurl + 'account/login', model);
+    return this.http.post(this.baseurl + 'account/login', model)
+      .pipe(
+        map((response: User) => {
+          const user = response;
+          if(user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+        })
+      );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
