7658332b3f101bda4a6c65dcb0534304904e1b63
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-01 12:31:05 +0200

    create basic routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { MessagesComponent } from './components/messages/messages.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
+import { MemberListComponent } from './components/members/member-list/member-list.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', component: MemberListComponent},
+  {path: 'members/:id', component: MemberDetailComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
