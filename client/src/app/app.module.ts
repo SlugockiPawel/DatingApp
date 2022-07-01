7b579eccf47e83533a3f3e4847cd2edc1cc8e345
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-01 18:32:04 +0200

    add shared module and move all non-angular related modules (like ngx-bootstrap) there
    for clean code purpose

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './_modules/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,14 +9,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component'
-import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -35,10 +34,8 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     TooltipModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
