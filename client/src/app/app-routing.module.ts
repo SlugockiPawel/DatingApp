cce040887c570de62839f00e55f9b63180c7d179
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-03 15:57:27 +0200

    add server-error component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -24,6 +25,7 @@ const routes: Routes = [
   },
   {path: 'errors', component: TestErrorsComponent},
   {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
   {path: '**', component: NotFoundComponent, pathMatch: 'full'},
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,8 +16,9 @@ import { MemberListComponent } from './components/members/member-list/member-lis
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component'
 
 @NgModule({
   declarations: [
@@ -29,8 +30,9 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
     NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
