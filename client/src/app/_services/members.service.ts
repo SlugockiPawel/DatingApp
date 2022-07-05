00addfad1868b639d9a371ce999550f63242d8b6
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-05 18:38:11 +0200

    implement getMembers(0 and getMember() in members service

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,29 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(): Observable<Member[]>{
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
