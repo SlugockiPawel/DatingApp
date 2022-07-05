acb6624bf4a87f12cacef5c9e9cbfe1035bf617f
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-05 11:41:41 +0200

    add member and photo dtos

---
+++ DatingApp/DTOs/MemberDto.cs
@@ -0,0 +1,23 @@
+﻿using DatingApp.Models;
+
+namespace DatingApp.DTOs
+{
+    public class MemberDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age{ get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Navigation property 1 User => many Photos
+        public virtual ICollection<PhotoDto> Photos { get; set; }
+    }
+}
