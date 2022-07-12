2ec13df0c0559830734d99314c741eb2e1137297
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-12 12:31:36 +0200

    add setMainPhoto funcionality

--- client/src/app/components/members/photo-editor/photo-editor.component.ts
+++ client/src/app/components/members/photo-editor/photo-editor.component.ts
@@ -5,6 +5,8 @@ import {environment} from "../../../../environments/environment";
 import {User} from "../../../_models/user";
 import {AccountService} from "../../../_services/account.service";
 import {take} from "rxjs";
+import {Photo} from "../../../_models/photo";
+import {MembersService} from "../../../_services/members.service";
 
 @Component({
   selector: 'app-photo-editor',
@@ -18,7 +20,7 @@ export class PhotoEditorComponent implements OnInit {
   baseUrl = environment.apiUrl;
   user: User;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private membersService: MembersService) {
     accountService.currentUser$.pipe(take<User>(1)).subscribe({
       next: user => this.user = user
     });
@@ -26,6 +28,19 @@ export class PhotoEditorComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializeUploader();
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.membersService.setMainPhoto(photo.id).subscribe(next => {
+      this.user.mainPhotoUrl = photo.url;
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+      this.member.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if(p.id === photo.id) p.isMain = true;
+        }
+      );
+    });
   }
 
   fileOverBase(e: any) {
