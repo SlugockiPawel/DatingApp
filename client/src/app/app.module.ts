7209aa82320e92eb31865a4116f4388a6533dd8c
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-13 17:24:37 +0200

    add ReactiveFormsModule, create reactive register form and add validations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -50,6 +49,7 @@ import { PhotoEditorComponent } from './components/members/photo-editor/photo-ed
     HttpClientModule,
     TooltipModule,
     FormsModule,
+    ReactiveFormsModule,
     NgxSpinnerModule,
     SharedModule
   ],
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
-import { ToastrService } from 'ngx-toastr';
-import { AccountService } from './../../_services/account.service';
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {AccountService} from './../../_services/account.service';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,23 +11,38 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) {
+  }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(6),
+                        Validators.maxLength(20)]),
+      confirmPassword: new FormControl('', Validators.required),
+    });
   }
 
   register() {
-    this.accountService.registerUser(this.model).subscribe({
-      next:  (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => {
-        console.log(error),
-        this.toastr.error(error.error);
-      }
-    });
+    console.log(this.registerForm.value);
+
+    // this.accountService.registerUser(this.model).subscribe({
+    //   next:  (response) => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: (error) => {
+    //     console.log(error),
+    //     this.toastr.error(error.error);
+    //   }
+    // });
   }
 
   cancel() {
