62a3e1073488775ef3cba46a0758b4dc81332404
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-16 12:27:00 +0200

    add PahinationHeader class

---
+++ DatingApp/Helpers/PaginationHeader.cs
@@ -0,0 +1,17 @@
+﻿namespace DatingApp.Helpers;
+
+public class PaginationHeader
+{
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalItems { get; set; }
+    public int ItemsPerPage { get; set; }
+
+    public PaginationHeader(int currentPage, int totalPages, int totalItems, int itemsPerPage)
+    {
+        CurrentPage = currentPage;
+        TotalPages = totalPages;
+        TotalItems = totalItems;
+        ItemsPerPage = itemsPerPage;
+    }
+}
