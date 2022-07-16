c3110e523dde3e5d445559443e2542460b401ef3
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-16 12:27:35 +0200

    add HttpExtensions class with AddPaginationHeader method

---
+++ DatingApp/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using DatingApp.Helpers;
+
+namespace DatingApp.Extensions;
+
+public static class HttpExtensions
+{
+    public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage,
+        int totalItems, int totalPages)
+    {
+        var paginationHeader = new PaginationHeader(currentPage, totalPages, totalItems, itemsPerPage);
+        response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+        response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+    }
+}
