8b6aa99f2850cf2455f7f275d2d2779095ba43fc
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-18 11:45:36 +0200

    add prettier and reformat code

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,41 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
-import { User } from '../_models/user';
-import { ReplaySubject } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {map, ReplaySubject} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {User} from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   private baseurl: string = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   registerUser(model: any) {
-    return this.http.post(this.baseurl + 'account/register', model)
-      .pipe(
-        map((user: User) => {
-          if(user) {
-            this.setCurrentUser(user);
-          }
-        })
-      )
+    return this.http.post(this.baseurl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
   }
 
   login(model: any) {
-    return this.http.post(this.baseurl + 'account/login', model)
-      .pipe(
-        map((response: User) => {
-          const user = response;
-          if(user) {
-            this.setCurrentUser(user);
-          }
-        })
-      );
+    return this.http.post(this.baseurl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
   }
 
   setCurrentUser(user: User) {
