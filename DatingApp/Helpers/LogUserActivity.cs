1ddca9bd613a0fc197f3e67b324a358aca0a4a99
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-18 20:14:17 +0200

    in LogUserActivity ActionFilter use GetUserByIdAsync() as it is quicker than GetUserByNameAsync()

--- DatingApp/Helpers/LogUserActivity.cs
+++ DatingApp/Helpers/LogUserActivity.cs
@@ -16,9 +16,9 @@ public class LogUserActivity : IAsyncActionFilter
         if (!resultContext.HttpContext.User.Identity.IsAuthenticated)
             return;
 
-        var username = resultContext.HttpContext.User.GetUserName();
+        var userId = resultContext.HttpContext.User.GetUserId();
         var repo = resultContext.HttpContext.RequestServices.GetService<IUserService>();
-        var user = await repo.GetUserByNameAsync(username);
+        var user = await repo.GetUserByIdAsync(userId);
         user.LastActive = DateTime.Now;
         await repo.SaveAllAsync();
     }
