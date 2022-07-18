8c1ce41acbd9f587b33d75ab5b94128cc01422eb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-18 20:13:27 +0200

    add GetUserId extension method to ClaimsPrinciple

--- DatingApp/Extensions/ClaimsPrincipleExtensions.cs
+++ DatingApp/Extensions/ClaimsPrincipleExtensions.cs
@@ -6,6 +6,13 @@ public static class ClaimsPrincipleExtensions
 {
     public static string GetUserName(this ClaimsPrincipal user)
     {
-        return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        // it represents UniqueName in tokenService
+        return user.FindFirst(ClaimTypes.Name)?.Value;
+    }
+
+    public static Guid GetUserId(this ClaimsPrincipal user)
+    {
+        // it represents UniqueName in tokenService
+        return new Guid(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
     }
 }
