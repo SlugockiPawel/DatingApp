759d72614d130b9f1c7f8fc91063838e85d4e2c4
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-19 19:39:19 +0200

    configure many-to-many relationship in AppUserLike table
    it is necessary when we has an additional table

--- DatingApp/Data/ApplicationDbContext.cs
+++ DatingApp/Data/ApplicationDbContext.cs
@@ -1,14 +1,35 @@
 ﻿using DatingApp.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DatingApp.Data
-{
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace DatingApp.Data;
 
-        public DbSet<AppUser> Users { get; set; }
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<AppUserLike> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AppUserLike>().HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+        modelBuilder
+            .Entity<AppUserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder
+            .Entity<AppUserLike>()
+            .HasOne(l => l.LikedUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(l => l.LikedUserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
