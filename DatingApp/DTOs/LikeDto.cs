bc08f9702dcf023c2e9ed0c7c2ff7d6a5ad1b588
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-19 21:50:49 +0200

    add LikeDto

---
+++ DatingApp/DTOs/LikeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DatingApp.DTOs;
+
+public class LikeDto
+{
+    public Guid UserId { get; set; }
+    public string name { get; set; }
+    public int Age { get; set; }
+    public string KnownAs { get; set; }
+    public string PhotoUrl { get; set; }
+    public string City { get; set; }
+}
