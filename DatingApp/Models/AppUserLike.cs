97b105ce52883a975656ce4a53c5c6ea4625a50b
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-19 19:36:11 +0200

    create ApppUserLike model (tabel for many-to-many relationship)

---
+++ DatingApp/Models/AppUserLike.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Models;
+
+public class AppUserLike
+{
+    public AppUser SourceUser { get; set; }
+    public Guid SourceUserId { get; set; }
+
+    public AppUser LikedUser { get; set; }
+    public Guid LikedUserId { get; set; }
+}
