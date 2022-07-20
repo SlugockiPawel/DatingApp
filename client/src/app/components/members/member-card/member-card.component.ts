8895b88ccb7506afa50ca13684dbaf5ad6947ac4
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-20 12:38:50 +0200

    add like functionality

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -1,18 +1,28 @@
-import { Member } from './../../../_models/member';
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {MembersService} from '../../../_services/members.service';
+import {Member} from './../../../_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
+  @Input() member: Member;
 
-  @Input() member: Member
-
-  constructor() { }
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  addLike(member: Member) {
+    this.memberService.addLike(member.name).subscribe(() => {
+      this.toastr.success('You have liked ' + member.knownAs);
+    });
+  }
 }
