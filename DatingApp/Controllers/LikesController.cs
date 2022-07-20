76491656b8d0bc70764c1b2e26eedbda47f4d7bf
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-20 10:52:18 +0200

    add LikesController with AddLike and GetUserLikes actions

---
+++ DatingApp/Controllers/LikesController.cs
@@ -0,0 +1,56 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Extensions;
+using DatingApp.Models;
+using DatingApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers;
+
+[Authorize]
+public class LikesController : BaseApiController
+{
+    private readonly ILikeService _likeService;
+    private readonly IUserService _userService;
+
+    public LikesController(ILikeService likeService, IUserService userService)
+    {
+        _likeService = likeService;
+        _userService = userService;
+    }
+
+    [HttpPost("{username}")]
+    public async Task<ActionResult> AddLike(string username)
+    {
+        var sourceUserId = User.GetUserId();
+        var likedUser = await _userService.GetUserByNameAsync(username);
+        var sourceUser = await _likeService.GetUserWithLikes(sourceUserId);
+
+        if (likedUser is null)
+            return NotFound();
+
+        if (sourceUser.Name.Equals(username))
+            return BadRequest("You cannot give yourself a like.");
+
+        var userLike = await _likeService.GetUserLike(sourceUserId, likedUser.Id);
+
+        if (userLike is not null)
+            return BadRequest("You already like this user");
+
+        userLike = new AppUserLike { SourceUserId = sourceUserId, LikedUserId = likedUser.Id };
+
+        sourceUser.LikedUsers.Add(userLike);
+
+        if (await _userService.SaveAllAsync())
+            return Ok();
+
+        return BadRequest("Failed to like user");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+    {
+        var users = await _likeService.GetUserLikes(predicate, User.GetUserId());
+        return Ok(users);
+    }
+}
