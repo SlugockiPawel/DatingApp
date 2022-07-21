dc930ab1a59f6759f71b9102647ad50bba66d83c
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-21 12:32:00 +0200

    add IMessageService

---
+++ DatingApp/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Helpers;
+using DatingApp.Models;
+
+namespace DatingApp.Services.Interfaces;
+
+public interface IMessageService
+{
+    void AddMessage(Message message);
+    void DeleteMessage(Message message);
+    Task<Message> GetMessage(int id);
+    Task<PagedList<MessageDto>> GetMessagesForUser();
+    Task<IEnumerable<MessageDto>> GetMessageThread(Guid currentUserId, Guid recipientId);
+    Task<bool> SaveAllAsync();
+}
