b6b5f0d552b8329c99a8bbe4579638f61be2d6c9
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-21 12:18:49 +0200

    add Message model

---
+++ DatingApp/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace DatingApp.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public Guid SenderId { get; set; }
+    public string SenderName { get; set; }
+    public AppUser Sender { get; set; }
+    public Guid RecipientId { get; set; }
+    public string RecipientName { get; set; }
+    public AppUser Recipient { get; set; }
+    public string Content { get; set; }
+    public DateTime? DateRead { get; set; }
+    public DateTime DateSent { get; set; } = DateTime.Now;
+    public bool SenderDeleted { get; set; }
+    public bool RecipientDeleted { get; set; }
+}
