b3806b6c86a547f4d56896e6c81b223e8785d653
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-22 17:14:35 +0200

    load messages in message component and display content in message.html

--- client/src/app/components/messages/messages.component.ts
+++ client/src/app/components/messages/messages.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Message} from '../../_models/message';
+import {Pagination} from '../../_models/pagination';
+import {MessageService} from '../../_services/message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Outbox';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly messageService: MessageService) {
   }
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService
+      .getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe((response) => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
