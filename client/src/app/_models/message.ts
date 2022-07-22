f85b790790d7a391cf15e2afbac0355f80570218
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-22 17:05:07 +0200

    add Message model

---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+﻿export interface Message {
+  id: number;
+  senderId: string;
+  senderName: string;
+  senderPhotoUrl: string;
+  recipientId: string;
+  recipientName: string;
+  recipientPhotoUrl: string;
+  content: string;
+  dateRead: Date;
+  dateSent: Date;
+}
