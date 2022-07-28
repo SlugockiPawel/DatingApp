267da59811a6fcd902436aa9cd3bc574dc3729ad
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-28 19:11:58 +0200

    add AdminsController as a base for Policy based authorization

---
+++ DatingApp/Controllers/AdminsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers;
+
+public class AdminsController : BaseApiController
+{
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpGet("users-with-roles")]
+    public IActionResult GetUsersWithRoles()
+    {
+        return Ok("Only admins can see this");
+    }
+
+    [Authorize(Policy = "ModeratePhotoRole")]
+    [HttpGet("photos-to-moderate")]
+    public IActionResult GetPhotosForModeration()
+    {
+        return Ok("Admins or moderators can see this");
+    }
+}
