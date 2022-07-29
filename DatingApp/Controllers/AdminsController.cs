27e1568e97962ab0ca192eb64099f74452d0c787
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-07-29 15:02:01 +0200

    add GetUsersWithRoles()

--- DatingApp/Controllers/AdminsController.cs
+++ DatingApp/Controllers/AdminsController.cs
@@ -1,16 +1,40 @@
 ﻿using DatingApp.Enums;
+using DatingApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Controllers;
 
 public class AdminsController : BaseApiController
 {
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminsController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     [Authorize(Policy = $"{nameof(AuthPolicies.RequireAdminRole)}")]
     [HttpGet("users-with-roles")]
-    public IActionResult GetUsersWithRoles()
+    public async Task<IActionResult> GetUsersWithRoles()
     {
-        return Ok("Only admins can see this");
+        var users = _userManager.Users
+            .Include(u => u.UserRoles)
+            .ThenInclude(ur => ur.Role)
+            .OrderBy(u => u.UserName)
+            .Select(
+                u =>
+                    new
+                    {
+                        u.Id,
+                        u.UserName,
+                        Roles = u.UserRoles.Select(ur => ur.Role.Name).ToList()
+                    }
+            );
+
+        return Ok(users);
     }
 
     [Authorize(Policy = $"{nameof(AuthPolicies.ModeratePhotoRole)}")]
