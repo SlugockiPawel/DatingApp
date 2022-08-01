904f0d54a7d79734a94124decb892318989a27b2
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-01 12:55:13 +0200

    implement user-management component

--- client/src/app/components/admin/user-management/user-management.component.ts
+++ client/src/app/components/admin/user-management/user-management.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../_models/user';
+import {AdminService} from '../../../_services/admin.service';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.css']
+  styleUrls: ['./user-management.component.css'],
 })
 export class UserManagementComponent implements OnInit {
+  users: Partial<User[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly adminService: AdminService) {
   }
 
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    return this.adminService.getUsersWithRoles().subscribe((users) => {
+      this.users = users;
+    });
+  }
 }
