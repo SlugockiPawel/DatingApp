2406d4171e50e44adbbf81b99b1d57afe2b082dd
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-01 12:53:11 +0200

    add admin service with getUsersWithRoles()

---
+++ client/src/app/_services/admin.service.ts
@@ -0,0 +1,20 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {User} from '../_models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  getUsersWithRoles() {
+    return this.http.get<Partial<User[]>>(
+      this.baseUrl + 'admins/users-with-roles'
+    );
+  }
+}
