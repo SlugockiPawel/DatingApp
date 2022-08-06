13190d7e8d2a3f0313f284930e0ef0f5e5247931
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-06 19:00:05 +0200

    add SendMessage()

--- DatingApp/SignalR/MessageHub.cs
+++ DatingApp/SignalR/MessageHub.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using DatingApp.DTOs;
 using DatingApp.Extensions;
+using DatingApp.Models;
 using DatingApp.Services.Interfaces;
 using Microsoft.AspNetCore.SignalR;
 
@@ -9,11 +11,13 @@ public class MessageHub : Hub
 {
     private readonly Mapper _mapper;
     private readonly IMessageService _messageService;
+    private readonly IUserService _userService;
 
-    public MessageHub(IMessageService messageService, Mapper mapper)
+    public MessageHub(IMessageService messageService, Mapper mapper, IUserService userService)
     {
         _messageService = messageService;
         _mapper = mapper;
+        _userService = userService;
     }
 
     public override async Task OnConnectedAsync()
@@ -32,6 +36,39 @@ public class MessageHub : Hub
     public override async Task OnDisconnectedAsync(Exception exception)
     {
         await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+        var username = Context.User.GetUserName();
+
+        if (username == createMessageDto.RecipientName.ToLower())
+            throw new HubException("You cannot send messages to yourself");
+
+        var sender = await _userService.GetUserByNameAsync(username);
+        var recipient = await _userService.GetUserByNameAsync(createMessageDto.RecipientName);
+
+        if (recipient is null)
+        {
+            throw new HubException("Not found user");
+        }
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderName = sender.UserName,
+            RecipientName = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+
+        await _messageService.AddMessageAsync(message);
+
+        if (await _messageService.SaveAllAsync())
+        {
+            var group = GetGroupName(sender.UserName, recipient.UserName);
+            await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+        }
     }
 
     private string GetGroupName(string callerUser, string otherUser)
