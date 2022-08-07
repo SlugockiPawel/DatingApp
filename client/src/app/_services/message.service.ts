0e6a787df35b2c193bc3da658dc81cce0e45e4a1
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-07 17:44:33 +0200

    add createHubConnection() and stopHubConnection()

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,7 +1,10 @@
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {HubConnection, HubConnectionBuilder} from '@microsoft/signalr';
+import {BehaviorSubject} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {Message} from '../_models/message';
+import {User} from '../_models/user';
 import {getPaginatedResult, getPaginationHeaders} from './paginationHelper';
 
 @Injectable({
@@ -9,12 +12,33 @@ import {getPaginatedResult, getPaginationHeaders} from './paginationHelper';
 })
 export class MessageService {
   baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private readonly http: HttpClient) {
   }
 
+  createHubConnection(user: User, otherUsername: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    this.hubConnection.on('ReceiveMessageThread', messages => this.messageThreadSource.next(messages));
+  }
+
+  stopHubConnection() {
+    this.hubConnection.stop();
+  }
+
   getMessages(pageNumber: number, pageSize: number, container: string) {
-    let params: HttpParams = getPaginationHeaders(pageNumber, pageSize);
+    let params = getPaginationHeaders(pageNumber, pageSize);
     params = params.append('Container', container);
     return getPaginatedResult<Message[]>(
       this.baseUrl + 'messages',
