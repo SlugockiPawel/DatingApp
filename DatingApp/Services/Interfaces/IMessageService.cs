d41bf0c744d764504a1ba5a63bccf0c881e33824
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-08 17:43:32 +0200

    add signalR related method signatures to IMessageService

--- DatingApp/Services/Interfaces/IMessageService.cs
+++ DatingApp/Services/Interfaces/IMessageService.cs
@@ -6,10 +6,20 @@ namespace DatingApp.Services.Interfaces;
 
 public interface IMessageService
 {
+    void AddGroup(Group group);
+    void RemoveConnection(Connection connection);
+    Task<Connection> GetConnection(string connectionId);
+    Task<Group> GetMessageGroup(string groupName);
+
     Task AddMessageAsync(Message message);
     void DeleteMessage(Message message);
     Task<Message> GetMessageAsync(int id);
     Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
-    Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
+
+    Task<IEnumerable<MessageDto>> GetMessageThread(
+        string currentUsername,
+        string recipientUsername
+    );
+
     Task<bool> SaveAllAsync();
 }
