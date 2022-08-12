7b7ec19d8c90eee8da4c5bc16b095f7a7293bfba
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-12 18:07:01 +0200

    add UnitOfWork

---
+++ DatingApp/Data/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DatingApp.Services;
+using DatingApp.Services.Interfaces;
+
+namespace DatingApp.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly ITokenService _tokenService;
+
+    public UnitOfWork(ApplicationDbContext context, IMapper mapper, ITokenService tokenService)
+    {
+        _context = context;
+        _mapper = mapper;
+        _tokenService = tokenService;
+    }
+
+    public IUserService UserService => new UserServicePostgres(_context, _tokenService, _mapper);
+    public IMessageService MessageService => new MessageService(_context, _mapper);
+    public ILikeService LikeService => new LikeService(_context);
+
+    public async Task<bool> Complete()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public bool HasChanges()
+    {
+        return _context.ChangeTracker.HasChanges();
+    }
+}
