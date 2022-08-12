c7757cf6c297066043f9ea42a48f8e0783e2613d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-12 18:06:42 +0200

    add IUnitOfWork

---
+++ DatingApp/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Services.Interfaces;
+
+public interface IUnitOfWork
+{
+    IUserService UserService { get; }
+    IMessageService MessageService { get; }
+    ILikeService LikeService { get; }
+    Task<bool> Complete();
+    bool HasChanges();
+}
