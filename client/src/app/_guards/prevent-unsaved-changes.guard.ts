0cf401fc2a0f42234d9fe534302becc04542aeb1
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-16 18:55:29 +0200

    display confirm dialog in prevent-unsaved-changes guard

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,17 +1,21 @@
 import {Injectable} from '@angular/core';
 import {CanDeactivate} from '@angular/router';
-import {MemberEditComponent} from "../components/members/member-edit/member-edit.component";
+import {Observable} from 'rxjs';
+import {ConfirmService} from '../_services/confirm.service';
+import {MemberEditComponent} from '../components/members/member-edit/member-edit.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(
-    component: MemberEditComponent): boolean {
-    if (component.editForm.dirty) {
-      return confirm("Are you sure you want to continue? Any unsaved changes will be lost");
-    }
-    return true;
+  constructor(private confirmService: ConfirmService) {
   }
 
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
+    if (component.editForm.dirty) {
+      return this.confirmService.confirm();
+    }
+
+    return true;
+  }
 }
--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -24,6 +26,23 @@ export class ConfirmService {
       },
     };
 
-    this.bsModalRef = this.modalService.show('Confirm', config);
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+
+    return new Observable<boolean>(this.getResult());
+  }
+
+  private getResult() {
+    return (observer) => {
+      const subscription = this.bsModalRef.onHidden.subscribe(() => {
+        observer.next(this.bsModalRef.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        },
+      };
+    };
   }
 }
