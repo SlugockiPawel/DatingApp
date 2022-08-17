8fe59c7afdca423b4cf8c69e2adb1417ce88e8d2
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-17 17:44:00 +0200

    add IsApproved prop to the Photo model

--- DatingApp/Models/Photo.cs
+++ DatingApp/Models/Photo.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DatingApp.Models
+namespace DatingApp.Models;
+
+[Table("Photos")]
+public class Photo
 {
-    [Table("Photos")]
-    public class Photo
-    {
-        public int Id { get; set; }
-        public string Url { get; set; }
-        public bool IsMain { get; set; }
-        public string PublicId { get; set; }
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string PublicId { get; set; }
+    public bool IsApproved { get; set; }
 
-        // Navigation property 1 User => many photos
-        public virtual AppUser AppUser { get; set; }
-        public Guid AppUserId { get; set; }
-
-    }
+    // Navigation property 1 User => many photos
+    public virtual AppUser AppUser { get; set; }
+    public Guid AppUserId { get; set; }
 }
