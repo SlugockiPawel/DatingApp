1ddf89cf06ba5fdddd24c675b26bf088f14a3eeb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-24 16:51:38 +0200

    add getPhotosForModeraton(), approvePhoto(), rejectPhoto() to PhotoManagementComponent

--- client/src/app/components/admin/photo-management/photo-management.component.ts
+++ client/src/app/components/admin/photo-management/photo-management.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Photo} from '../../../_models/photo';
+import {AdminService} from '../../../_services/admin.service';
 
 @Component({
   selector: 'app-photo-management',
   templateUrl: './photo-management.component.html',
-  styleUrls: ['./photo-management.component.css']
+  styleUrls: ['./photo-management.component.css'],
 })
 export class PhotoManagementComponent implements OnInit {
+  photosToApprove: Photo[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly adminService: AdminService) {
   }
 
+  ngOnInit() {
+  }
+
+  getPhotosForModeration() {
+    this.adminService.getPhotosForModeration().subscribe((photos: Photo[]) => {
+      this.photosToApprove = photos;
+    });
+  }
+
+  approvePhoto(photoId: number) {
+    this.adminService.approvePhoto(photoId).subscribe(() =>
+      this.photosToApprove.splice(
+        this.photosToApprove.findIndex(p => p.id === photoId),
+        1
+      ));
+  }
+
+  rejectPhoto(photoId: number) {
+    this.adminService.rejectPhoto(photoId).subscribe(() =>
+      this.photosToApprove.splice(
+        this.photosToApprove.findIndex(p => p.id === photoId)
+      ));
+  }
 }
