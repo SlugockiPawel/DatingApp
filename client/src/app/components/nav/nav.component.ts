2eefd2c3bff5c4981b384fbd2b00b98281113144
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-08-28 11:59:07 +0200

    lint code

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,33 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
-import { User } from 'src/app/_models/user';
-import { AccountService } from '../../_services/account.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AccountService} from '../../_services/account.service';
 import {Roles} from '../../Enums/roles';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
   rolesEnum = Roles;
 
+  constructor(public accountService: AccountService, private router: Router) {
+  }
 
-  constructor(public accountService: AccountService, private router: Router,
-    private toastr: ToastrService) { }
-
-  ngOnInit(): void {
-
+  ngOnInit() {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => this.router.navigateByUrl('/members'),
-      }
-    );
+      next: () => this.router.navigateByUrl('/members'),
+    });
   }
 
   logout() {
