657427347f9984346efb4ae20556fe987255b49e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-09-07 13:28:02 +0200

    add LikedByCurrentUser prop to display black heart for users that are liked by currently logged user

--- DatingApp/Services/LikeService.cs
+++ DatingApp/Services/LikeService.cs
@@ -32,13 +32,14 @@ public class LikeService : ILikeService
     public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
     {
         var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
-
         var likes = _context.Likes.AsQueryable();
+        IQueryable<LikeDto> likedUsers;
+        var loggedUserLikes = likes.Where(like => like.SourceUserId.Equals(likesParams.UserId));
 
         switch (likesParams.Predicate)
         {
             case "liked":
-                likes = likes.Where(like => like.SourceUserId.Equals(likesParams.UserId));
+                likes = loggedUserLikes;
                 users = likes.Select(like => like.LikedUser);
                 break;
             case "likedBy":
@@ -47,7 +48,45 @@ public class LikeService : ILikeService
                 break;
         }
 
-        var likedUsers = users.Select(
+        likedUsers = AddLikedByCurrentUserFlag(
+            likesParams.Predicate,
+            users,
+            loggedUserLikes,
+            likesParams.UserId
+        );
+
+        return await PagedList<LikeDto>.CreateAsync(
+            likedUsers,
+            likesParams.PageNumber,
+            likesParams.PageSize
+        );
+    }
+
+    private static IQueryable<LikeDto> AddLikedByCurrentUserFlag(
+        string predicate,
+        IQueryable<AppUser> users,
+        IQueryable<AppUserLike> loggedUserLikes,
+        Guid loggedUserId
+    )
+    {
+        if (predicate == "liked")
+        {
+            return users.Select(
+                u =>
+                    new LikeDto
+                    {
+                        UserId = u.Id,
+                        UserName = u.UserName,
+                        KnownAs = u.KnownAs,
+                        Age = u.DateOfBirth.CalculateAge(),
+                        PhotoUrl = u.Photos.FirstOrDefault(p => p.IsMain).Url,
+                        City = u.City,
+                        LikedByCurrentUser = true
+                    }
+            );
+        }
+
+        return users.Select(
             u =>
                 new LikeDto
                 {
@@ -56,14 +95,9 @@ public class LikeService : ILikeService
                     KnownAs = u.KnownAs,
                     Age = u.DateOfBirth.CalculateAge(),
                     PhotoUrl = u.Photos.FirstOrDefault(p => p.IsMain).Url,
-                    City = u.City
+                    City = u.City,
+                    LikedByCurrentUser = u.LikedByUsers.Any(ul => ul.SourceUserId == loggedUserId)
                 }
-        );
-
-        return await PagedList<LikeDto>.CreateAsync(
-            likedUsers,
-            likesParams.PageNumber,
-            likesParams.PageSize
         );
     }
 }
