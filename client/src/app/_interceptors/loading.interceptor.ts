f4a9871b5dd88c1694930672212aa3b68a8d4f5b
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-09-13 16:40:59 +0200

    remove delay from loading interceptor

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -1,25 +1,20 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import {delay, finalize, Observable} from 'rxjs';
-import {BusyService} from "../_services/busy.service";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {finalize, Observable} from 'rxjs';
+import {BusyService} from '../_services/busy.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private busyService: BusyService) {
+  }
 
-  constructor(private busyService: BusyService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
-      finalize(() => {
-        this.busyService.idle();
-      })
+      finalize(() => this.busyService.idle())
     );
   }
 }
